37f5d3dce6798fc49f7d18cbfe9d35c409948cfd
Brian Haller <brianshaller@gmail.com>
2018-04-30 10:19:40 -0700

    commit

---
+++ bubbleSort/index.js
@@ -0,0 +1,11 @@
+//Bubble sort
+// using a nested for loop, check each pair of elements, and put them in order.
+// Every pass will correctly position the last element, so you can reduce the size of the inner loop by 1 on each pass.
+// it has time complexity of O(n^2)
+// it is a stable sort, meaning that equal valued elements stay in their original order
+// Bubble sort is very ineffecient so it is never used in practice - insertion sort is
+// always better than bubble sort.
+
+function bubbleSort(arr) {}
+
+module.exports = bubbleSort;
