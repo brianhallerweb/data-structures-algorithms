7f885d01fe0023b8402e5520c7baf005f0d59853
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2018-05-14 10:16:00 -0700

    commit

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,34 +8,6 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  if (typeof stringA !== "string" || typeof stringB !== "string") {
-    throw new Error("input must be a string");
-  }
-  function objectMap(str) {
-    obj = {};
-    for (let char of str.toLowerCase().match(/\w/g)) {
-      if (obj[char]) {
-        obj[char] += 1;
-      } else {
-        obj[char] = 1;
-      }
-    }
-    return obj;
-  }
-  let objA = objectMap(stringA);
-  let objB = objectMap(stringB);
-
-  if (Object.keys(objA).length !== Object.keys(objB).length) {
-    return false;
-  }
-
-  for (let char in objA) {
-    if (objA[char] !== objB[char]) {
-      return false;
-    }
-  }
-  return true;
-}
+function anagrams(stringA, stringB) {}
 
 module.exports = anagrams;
