49257312ba34682a5a8211089e9a7f82ed374cf7
Brian Haller <brianshaller@gmail.com>
2018-09-20 14:30:53 -0600

    commit

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -1,6 +1,9 @@
 // --- Directions
-// Write a function that returns fizz, buzz, or a number input
+// Write a function that returns Fizz, Buzz, FizzBuzz or a number input
+// throw an error if input is not a number
 
-function fizzBuzz(n) {}
-
+// @format
+function fizzBuzz() {
+  return 'hello';
+}
 module.exports = fizzBuzz;
